7c3b761bf3e17ba6de74adef50f96b015fb44e05
Sebastian Otto <44194014+Bineryss@users.noreply.github.com>
2024-09-03 20:03:19 +0000

    add first prototype

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <script setup lang="ts">
+import SmartToast from './components/SmartToast.vue'
 
+const text = 'lorem ipsum dolorem isage regrabo cano lorem lorem ipsum dolorem isage regrabo cano lorem ipsum dolorem isage regrabo cano ipsum dolorem isage regrabo cano'
 </script>
 
 <template>
+	<h1>hello</h1>
+	<SmartToast class="top" type="success" :content="text" />
+	<SmartToast type="success" :content="text" />
 </template>
 
-<style scoped></style>
+<style scoped>
+.top {
+	bottom: 200px;
+}
+</style>
---
+++ src/components/SmartToast.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import { PhCheckCircle } from '@phosphor-icons/vue'
+
+defineProps<{
+	type: 'success' | 'error' | 'info'
+	content: string
+}>()
+</script>
+
+<template>
+	<div class="container">
+		<PhCheckCircle class="icon" :size="32" weight="bold" />
+		<div class="text-area">
+			<h3>{{ content }}</h3>
+		</div>
+	</div>
+</template>
+
+<style lang="css" scoped>
+.container {
+	margin-inline: 8px;
+	position: absolute;
+	left: 0;
+	bottom: 8px;
+	background-color: green;
+	border-radius: 4px;
+	display: grid;
+    grid-template-columns: 64px 0;
+	overflow: clip;
+    transition: 300ms;
+
+	.icon {
+		padding: 16px;
+	}
+
+	.text-area {
+		display: none;
+		border-radius: 4px;
+		padding-inline: 8px;
+        overflow: clip;
+
+		h3 {
+			margin-block: 8px;
+            text-align: left;
+		}
+	}
+
+	&:hover {
+		aspect-ratio: unset;
+		grid-template-columns: 64px calc(100vw - 80px);
+		height: fit-content;
+
+		.text-area {
+			display: inline;
+            overflow: clip;
+
+            h3 {
+                overflow: clip;
+            }
+		}
+	}
+}
+</style>
